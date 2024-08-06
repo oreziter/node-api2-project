4e4321f26eaf926a90af67fdf8bdca09dac21612
Daniel Ozotha <oreziter@gmail.com>
2024-08-06 12:43:34 -0500

    test

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // require your server and launch it here
+console.log('welocome back')
+
+const server = require('./api/server')
+
+const PORT =  9000 
+
+server.listen(PORT, () => {
+  console.log('listening on', PORT)
+})
