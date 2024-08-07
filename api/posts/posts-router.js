a0f3df2443d88349a76621c9acbd45aa1cdac26f
Daniel Ozotha <oreziter@gmail.com>
2024-08-06 22:43:44 -0500

    test

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -3,19 +3,6 @@ const Post = require('./posts-model');
 
 const router = express.Router()
 
-// router.get('/api/posts', async (req, res) => {
-//   try {
-//     const posts = await Post.find();
-//     res.status(200).json(posts);
-//   } catch (err) {
-//     res.status(500).json({
-//       message: "The posts information could not be retrieved",
-//       err: err.message,
-//       stack: err.stack,
-//     });
-//   }
-// });
-
 router.get('/', (req, res) => {
   Post.find()
   .then(found => {
@@ -131,11 +118,24 @@ try {
 }
 
 })
-
-
-router.get('/:id/comments', (req, res) => {
-
-})
-
+router.get('/:id/comments', async (req, res) => {
+  try {
+      const post = await Post.findById(req.params.id)
+      if (!post) {
+        res.status(404).json({
+          message: "The post with the specified ID does not exist", 
+        });
+      } else {
+        const comments = await Post.findPostComments(req.params.id);
+         res.status(200).json(comments);
+      }
+  } catch (err) {
+    res.status(500).json({ 
+      message: "The comments information could not be retrieved" , 
+        err: err.message,
+        stack: err.stack,
+      });
+    }
+});
 
 module.exports = router
